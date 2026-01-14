bf250a7b6a9fce97ace01c81b670acb8057b3cc8
Praneeth0526 <ph.dev58@gmail.com>
2026-01-15 02:36:15 +0530

    Update in Members list

--- src/templates/member-list.tsx
+++ src/templates/member-list.tsx
@@ -5,7 +5,11 @@ import MemberItem from "../components/item-member"
 import Pagination from "../components/pagination"
 
 export default function memberList({ data, pageContext, location }) {
-    const memberItems = data.allMdx.edges.map(item => (
+    const currentMemberItems = data.currentMembers.edges.map(item => (
+        <MemberItem data={item.node} key={item.node.id} />
+    ))
+
+    const formerMemberItems = data.formerMembers.edges.map(item => (
         <MemberItem data={item.node} key={item.node.id} />
     ))
 
@@ -19,10 +23,17 @@ export default function memberList({ data, pageContext, location }) {
             <div className="container mx-auto py-12">
                 <div className="title py-12 text-center">
                     <h2 className="font-black text-5xl text-color-1">
-                        Members
+                        Current Members
                     </h2>
                 </div>
-                <div className="flex flex-wrap">{memberItems}</div>
+                <div className="flex flex-wrap">{currentMemberItems}</div>
+                
+                <div className="title py-12 text-center mt-12">
+                    <h2 className="font-black text-5xl text-color-1">
+                        Former Members
+                    </h2>
+                </div>
+                <div className="flex flex-wrap">{formerMemberItems}</div>
                 <Pagination pageContext={pageContext} type="members" />
             </div>
         </Layout>
@@ -31,9 +42,42 @@ export default function memberList({ data, pageContext, location }) {
 
 export const query = graphql`
     query MemberListQuery($skip: Int!, $limit: Int!) {
-        allMdx(
-            filter: { fields: { sourceName: { eq: "member" } } }
+        currentMembers: allMdx(
+            filter: { 
+                fields: { sourceName: { eq: "member" } }
+                frontmatter: { role: { in: ["Member", "Mentor", "Moderator"] } }
+            }
             sort: { order: DESC, fields: frontmatter___role }
+        ) {
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        fullname
+                        role
+                        avatar {
+                            publicURL
+                            childImageSharp {
+                                fluid(maxWidth: 800) {
+                                    srcSet
+                                    ...GatsbyImageSharpFluid
+                                }
+                                id
+                            }
+                        }
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+        formerMembers: allMdx(
+            filter: { 
+                fields: { sourceName: { eq: "member" } }
+                frontmatter: { role: { eq: "Alumni" } }
+            }
+            sort: { order: DESC, fields: frontmatter___batch }
             limit: $limit
             skip: $skip
         ) {
